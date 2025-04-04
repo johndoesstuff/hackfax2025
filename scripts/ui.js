1f1b13ec487e53a4a0caf80d0c369b41e4624abf
johndoesstuff <johndoesstuff03@gmail.com>
2025-04-04 18:19:46 -0400

    canvas size update

---
+++ scripts/ui.js
@@ -0,0 +1,11 @@
+function updateCanvasSize() {
+	const canvas = document.getElementById('canvas');
+
+	canvas.width = canvas.offsetWidth;
+	canvas.height = canvas.offsetHeight;
+}
+
+updateCanvasSize();
+
+window.addEventListener('resize', updateCanvasSize);
+
